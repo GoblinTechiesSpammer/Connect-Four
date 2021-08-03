40d6e59851b8cb1c23bddb7c2853676d774ec977
Bailey <bailey.gage.davis@gmail.com>
2021-08-03 16:03:18 +0900

    makeBoard progress

--- connect4.js
+++ connect4.js
@@ -5,11 +5,11 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
@@ -17,6 +17,15 @@ var board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  arr = [];
+
+  for (let i = 0; i < HEIGHT; i++) {
+    arr[i] = [];
+    for (let j = 0; j < WIDTH; j++) {
+      arr[i][j] = [];
+      
+    }
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
